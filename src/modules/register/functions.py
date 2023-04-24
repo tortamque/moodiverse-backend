5b87a56d30a30221011b341d08caedd938377a8d
tortamque <90132962+tortamque@users.noreply.github.com>
2023-04-24 22:37:41 +0300

    Add a random avatar choice when registering a user (#20)

--- src/modules/register/functions.py
+++ src/modules/register/functions.py
@@ -1,10 +1,10 @@
 from datetime import datetime
 
 from src.config import engine
-from src.models.database import User
+from src.models.database import User, Avatar
 
 from sqlalchemy.orm import Session
-
+from sqlalchemy.sql.expression import func
 
 def check_email_and_username(email, username):
     with Session(engine) as session:
@@ -14,12 +14,15 @@ def check_email_and_username(email, username):
 
 def add_user_into_db(username, email, hashed_password, birthdate, registration_date):
     with Session(engine) as session:
+        random_avatar_id = session.query(Avatar.id).order_by(func.random()).limit(1).one()[0]
+
         user = User(
             username=username,
             email=email,
             password=hashed_password,
             birth_date=datetime.strptime(birthdate, '%d.%m.%Y'),
-            registration_date=datetime.strptime(registration_date, '%d.%m.%Y')
+            registration_date=datetime.strptime(registration_date, '%d.%m.%Y'),
+            avatar_id=random_avatar_id
         )
 
         session.add(user)
